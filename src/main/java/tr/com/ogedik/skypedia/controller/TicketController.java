f7fa3ec1da45320a3c474c0f11b5725b0b1d85c8
Orkun Gedik <orkungedik90@gmail.com>
2020-08-06 20:28:03 +0300

    javadoc added

--- src/main/java/tr/com/ogedik/skypedia/controller/TicketController.java
+++ src/main/java/tr/com/ogedik/skypedia/controller/TicketController.java
@@ -23,6 +23,12 @@ public class TicketController extends SkypediaController<TicketRequest, Ticket> 
         super(service, mapper, TicketController.class);
     }
 
+    /**
+     * Ticket bulk create controller. Creates ticket as many as available seats in that fly.
+     *
+     * @param request ticket creation request
+     * @return {@link SkypediaResponse}
+     */
     @PostMapping(PathConstants.BULK)
     public SkypediaResponse bulkCreate(@RequestBody TicketRequest request) {
         logger.info("A request has been received to bulk create ticket records for flight: {}", request.getFlightId());
